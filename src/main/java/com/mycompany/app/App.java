220042b2d5d39711f3f15418af7db085bce6ec3d
Vaishali0604 <42200478+Vaishali0604@users.noreply.github.com>
2019-01-31 11:57:14 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,62 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Scanner;
+public class App{
+
+public static void main(String args[]) {
+    float a, b, res;
+    char choice, ch;
+    Scanner scan = new Scanner(System.in);
+    do{
+       
+       System.out.print("1. ADD TWO NUMBERS\n");
+       System.out.print("2. SUBTRACT TWO NUMBERS\n");
+       System.out.print("3. MULTIPLY TWO NUMBERS\n");
+       System.out.print("4. DIVIDE TWO NUMBERS\n");
+       System.out.print("5. EXIT\n\n");
+       //System.out.print("Enter your choice : ");
+       choice = scan.next().charAt(0);
+       choice=5
+       switch(choice){
+          case '1': System.out.print("Enter Two Numbers\n");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    res = a+b;
+                    System.out.print("Result = "+res);
+                    break;
+
+          case '2': System.out.print("Enter Two Numbers\n");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    res = a-b;
+                    System.out.print("Result = "+res);
+                    break;
+
+          case '1': System.out.print("Enter Two Numbers\n");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    res = a*b;
+                    System.out.print("Result = "+res);
+                    break;
+
+          case '1': System.out.print("Enter Two Numbers\n");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    res = a/b;
+                    System.out.print("Result = "+res);
+                    break;
+
+          case '5': System.exit(0);
+                    break;
+
+          default : System.out.print("Invalid Choice !!!");
+                    break;
+
+        }
+        
+
+        System.out.println("\n------------------------------------------\n");
+      }while(choice!=2);
+}
 }
